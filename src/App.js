adc22ee5c0d0de6a73d3cb82312bdd6671b0c660
AJIT KUMAR PANDIT <ajitkumarpandit1999@gmail.com>
2023-10-02 17:47:34 +0530

    v form right and left side pannel v1.2.1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './Pages/Register/Register';
-
+import Category from './Pages/Category/Category';
+import { useState } from 'react';
 
 function App() {
+
+  
+    const storedData = JSON.parse(localStorage.getItem("data")) || {
+      name: "",
+      userName: "",
+      email: "",
+      mobile: "",
+      reg:""
+    };
+    const [data, setData] = useState(storedData);
+
   return (
     <>
         <Router>
           <div>
             <Routes>
               <Route path="/" element={<Register />}  />
-            </Routes>
+              {/* {data.reg &&  */}
+              {/* <Route path="/category" element={<Category />} /> */}
+              {/* } */}
+           <Route path="/category" element={<Category />} />
+              </Routes>
           </div>
-        </Router>
+        </Router>      
     </>
   );
 }
--- src/Components/Register/Form/Form.jsx
+++ src/Components/Register/Form/Form.jsx
@@ -1,19 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { redirect, useNavigate } from "react-router-dom";
 
 import StylesForm from "./Form.module.css";
 
 const Form = () => {
+  const navigate = useNavigate();
+
+  const storedData = JSON.parse(localStorage.getItem("data")) || {
+    name: "",
+    userName: "",
+    email: "",
+    mobile: "",
+    reg: false,
+  };
+  const [data, setData] = useState(storedData);
+
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(data));
+  }, [data]);
+
+  // useEffect(() => {
+  //   //function to be executed on page load
+  //   console.log("Function executed on page load", data.reg);
+  //   if (data.reg === "true") {
+  //     navigate("/category");
+  //   }
+  // }, []);
+
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+    if (data.reg) {
+      navigate('/category'); // Redirect to category
+    }
+  }, [navigate]);
+
   const [isError, setIsError] = useState(false);
   const [name, setName] = useState("");
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [mobile, setMobile] = useState("");
   const [isChecked, setIsChecked] = useState(false);
+  const [reg, setReg] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name);
 
+    if (handelError() === false) {
+      updateData();
+      console.log("set data reg before true ", data.reg);
+      
+      storedData.reg = true; 
+      console.log("set data reg after true ", data.reg);
+      navigate("/category");
+    } else {
+      console.log("set data reg before false ", data.reg);
+      storedData.reg = false;
+      console.log("set data reg after false ", data.reg);
+      console.log("set data reg", data.reg);
+    }
+    // console.log("upadate data", data);
+    // console.log(data.reg);
     handelError();
   };
 
@@ -28,103 +75,167 @@ const Form = () => {
       email.length <= 0 ||
       mobile.length <= 0 ||
       !isChecked ||
-      !isValidEmail(email)||
-      !isMobileValid(mobile)
+      !isValidEmail(email) ||
+      !isMobileValid(mobile) ||
+      !isUserNameValid(userName) ||
+      !isName(name)
     ) {
       setIsError(true);
-      console.log("isError ", isError, "email ", email);
+      return true;
+      // console.log("isError ", isError, "email ", email);
     } else {
       setIsError(false);
+      return false;
     }
 
     // email error check email valadation
   };
 
-  const showError = () => {};
+  // const showError = () => {};
 
-  const isValidEmail=(email) =>{
+  const isName = (name) => {
+    const validNameRegex = /^[a-zA-Z ]{3,}$/;
+    return validNameRegex.test(name);
+  };
+
+  const isValidEmail = (email) => {
     const validateEmailRegex = /^\S+@\S+\.\S+$/;
     return validateEmailRegex.test(email);
   };
 
   const isMobileValid = (mobile) => {
-    const validMobileRegex = /^\d{10}$/;
+    const validMobileRegex = /^\+?[1-9][0-9]{8,9}$/;
     return validMobileRegex.test(mobile);
   };
 
-  const isUserNameValid =(userName)=>{
-    const validUserNameRegex = /^[a-zA-Z]{3,10}$/;
+  const isUserNameValid = (userName) => {
+    const validUserNameRegex = /^[a-zA-Z0-9]{3,10}$/;
     return validUserNameRegex.test(userName);
-  }
+  };
+
+  const updateData = () => {
+    data.name = name;
+    data.userName = userName;
+    data.email = email;
+    data.mobile = mobile;
+  };
 
   return (
     <>
+      {/* // console.log("data.reg", data.reg) */}
+      {/* redirect.redirect("/category"); */}
+
       <div>
         <form onSubmit={handleSubmit}>
           <br />
           <br />
+          {/* [Name]****************************************************************************** */}
           <div>
-            <input
-              className={`${StylesForm.inputStyle}`}
-              type="text"
-              placeholder="Name"
-              onChange={(e) => setName(e.target.value)}
-            />
-            {isError && name.length <= 0 ? (
+            {isError && !isName(name) ? (
+              <input
+                className={`${StylesForm.inputStyle}, ${StylesForm.errorBox}`}
+                type="text"
+                placeholder="Name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            ) : (
+              <input
+                className={`${StylesForm.inputStyle}`}
+                type="text"
+                placeholder="Name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            )}
+            {isError && !isName(name) ? (
               <label className={`${StylesForm.error}`}>
                 <br />
-                Field is required
+                Field is required must be albhabet
               </label>
             ) : null}
           </div>
           <br />
+          {/* [UserName]****************************************************************************** */}
+
           <div>
-            <input
-              className={`${StylesForm.inputStyle}`}
-              type="text"
-              placeholder="UserName"
-              onChange={(e) => setUserName(e.target.value)}
-            />
+            {isError && !isUserNameValid(userName) ? (
+              <input
+                className={`${StylesForm.inputStyle}, ${StylesForm.errorBox}`}
+                type="text"
+                placeholder="UserName"
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            ) : (
+              <input
+                className={`${StylesForm.inputStyle}`}
+                type="text"
+                placeholder="UserName"
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            )}
             {isError && !isUserNameValid(userName) ? (
               <label className={`${StylesForm.error}`}>
                 <br />
-                Field is required
+                Field is required [ without space must be 3-10 char ]
               </label>
             ) : null}
           </div>
           <br />
+          {/* [Email]****************************************************************************** */}
+
           <div>
-            <input
-              className={`${StylesForm.inputStyle}`}
-              type="text"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            {isError && !isValidEmail(email) ? (
+              <input
+                className={`${StylesForm.inputStyle}, ${StylesForm.errorBox}`}
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            ) : (
+              <input
+                className={`${StylesForm.inputStyle}`}
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            )}
             {isError && !isValidEmail(email) ? (
               <label className={`${StylesForm.error}`}>
                 <br />
-                Field is required
+                Field is required [ must be valid email format ]
               </label>
             ) : null}
           </div>
           <br />
+          {/* [Mobile]****************************************************************************** */}
+
           <div>
-            <input
-              className={`${StylesForm.inputStyle}`}
-              type="number"
-              placeholder="Mobile"
-              onChange={(e) => setMobile(e.target.value)}
-            />
+            {isError && !isMobileValid(mobile) ? (
+              <input
+                className={`${StylesForm.inputStyle}, ${StylesForm.errorBox}`}
+                type="number"
+                placeholder="Mobile"
+                onChange={(e) => setMobile(e.target.value)}
+              />
+            ) : (
+              <input
+                className={`${StylesForm.inputStyle}`}
+                type="number"
+                placeholder="Mobile"
+                onChange={(e) => setMobile(e.target.value)}
+              />
+            )}
             {isError && !isMobileValid(mobile) ? (
               <label className={`${StylesForm.error}`}>
                 <br />
-                Field is required
+                Field is required [ must be 9 or 10 digit number ]
               </label>
             ) : null}
           </div>
           <br />
-          <br />
+          {/* [Chekbox]****************************************************************************** */}
+
           <div>
             <label>
               <input
@@ -145,7 +256,8 @@ const Form = () => {
             ) : null}
           </div>
           <br />
-          <br />
+          {/* [SubmitButton]****************************************************************************** */}
+
           <div>
             <button className={StylesForm["green-button-sign-up"]}>
               SIGN UP
