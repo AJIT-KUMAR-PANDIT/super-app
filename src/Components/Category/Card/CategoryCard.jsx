1476cbe713d0ed7a341a77a26fdd44e8aef3e949
AJIT KUMAR PANDIT <ajitkumarpandit1999@gmail.com>
2023-10-03 20:24:00 +0530

    pics

--- src/Components/Category/Card/CategoryCard.jsx
+++ src/Components/Category/Card/CategoryCard.jsx
@@ -1,23 +1,19 @@
-import StyleCategoryCard from '../../../styles/CategoryCard.module.scss';
+import StyleCategoryCard from "../../../styles/CategoryCard.module.scss";
 
 const CategoryCard = (props) => {
+  const { type, color, image } = props;
 
-  const {type,color,image}=props;
-  
-// Dynamically update the CSS variable
-document.documentElement.style.setProperty('--background-color', color);
-document.documentElement.style.setProperty('--image', `url(${image})`);
-
+  // Dynamically update the CSS variable
+  document.documentElement.style.setProperty("--background-color", color);
+  document.documentElement.style.setProperty("--image", `url(${image})`);
+console.log(image);
+console.log(`url(${image})`);
   return (
-    <div className={`${StyleCategoryCard.container}`}>{type}
-    <div className={`${StyleCategoryCard.imageDiv}`}>
-
-      
-dhjdf
-defaultdf
+    <div className={`${StyleCategoryCard.container}`}>
+      {type}
+      <div className={`${StyleCategoryCard.imageDiv}`}>dhjdf defaultdf</div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default CategoryCard
+export default CategoryCard;
